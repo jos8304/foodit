2db77ead8014ddf06df66a99afb8244025646626
Jason <jos8304@hotmail.com>
2024-06-06 14:21:28 -0400

    2

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
 import FoodList from "./FoodList"
 import items from '../mock.json'
+import { useState } from "react";
 
 function App() {
-    return (<div><FoodList items={items} /></div>) 
+    const [order, setOrder] = useState('createdAt')
+    const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+    const handleCalorieClick = () => setOrder('calorie')
+    const handleNewClick = () => setOrder('createdAt')
+
+    return (
+        <div>
+            <div>
+                <button onClick={handleNewClick}>최신순</button>
+                <button onClick={handleCalorieClick}>칼로리순</button>
+            </div>
+            <FoodList items={sortedItems} />
+        </div>
+    ) 
 }
+
+
 
 export default App
--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,6 +1,26 @@
+function formatDate(value){
+    const date = new Date(value)
+    return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`
+}
+
+function FoodListItem({item}){
+    return(
+        <div>
+            <img src={item.imgUrl}></img>
+            <div>
+                <h1>{item.title}</h1>                
+                <p>{item.calorie}</p>
+                <p>{formatDate(item.createdAt)}</p>
+                <p>{item.content}</p>
+            </div>
+        </div>
+
+    )
+}
+
 function FoodList({items}){
     return( <ul>{items.map((item) =>{
-        return <li>{item.title}</li>
+        return <li><FoodListItem item={item} /></li>
     })}
     </ul>
     )
